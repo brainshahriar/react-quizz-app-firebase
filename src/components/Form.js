0b2feb290b352ddb3c0381c04814a069fc22964d
brain <shahriarmehedi95@gmail.com>
2021-12-07 00:47:01 +0600

    registration form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
+import classes from "../styles/Form.module.css";
 
-
-export default function Form(){
-    return (
-            <>
-            
-            </>
-    );
+export default function Form({ children, className, ...rest }) {
+  return (
+    <form className={`${className} ${classes.form}`} action="#" {...rest}>
+      {children}
+    </form>
+  );
 }
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,13 +1,16 @@
 import Form from "../Form";
 import Illustration from "../Illustration";
 import classes from "../../styles/Signup.module.css"
+import TextInput from "../TextInput";
 export default function Signup(){
     return(
         <>
              <h1>Create an account</h1>
              <div className="column"> 
                     <Illustration />
-                    <Form className={`${classes.signup}form`} />
+                    <Form className={`${classes.signup}`}>
+                        <TextInput />
+                        </Form>
              </div>
         </>
     );
